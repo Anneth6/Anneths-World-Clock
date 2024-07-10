5727ae1da27e2840e6b3ef04da691fac3d869948
Anneth6 <annrussell@outlook.com>
2024-07-10 13:21:08 +1000

    Add home button
    Need to prevent theme colour change in future.

--- src/script.js
+++ src/script.js
@@ -2,5 +2,13 @@ function changeTheme() {
   let body = document.querySelector("body");
   body.classList.toggle("dark");
 }
+
+function homePage() {
+  window.location.href = "index.html";
+}
+
 let themeButton = document.querySelector(".themeButton");
 themeButton.addEventListener("click", changeTheme);
+
+let returnButton = document.querySelector(".returnButton");
+returnButton.addEventListener("click", homePage);
