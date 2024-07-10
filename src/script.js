c7e9ad6eaca2604c864d173fd1b9ca0c4608ccb7
Anneth6 <annrussell@outlook.com>
2024-07-10 13:36:55 +1000

    Commented out JS code
    Continued work on home button preventing theme change

--- src/script.js
+++ src/script.js
@@ -5,10 +5,16 @@ function changeTheme() {
 
 function homePage() {
   window.location.href = "index.html";
+
+  if (changeTheme === ".dark") {
+    body.classList.toggle("dark");
+  }
 }
 
+// set page theme
 let themeButton = document.querySelector(".themeButton");
 themeButton.addEventListener("click", changeTheme);
 
+// set return home button
 let returnButton = document.querySelector(".returnButton");
 returnButton.addEventListener("click", homePage);
